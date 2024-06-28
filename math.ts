161c289e003282ab5eb723672a9167c0df92c2f2
divoglav <divoglav@protonmail.com>
2024-06-29 00:09:31 +0300

    feat: math

---
+++ math.ts
@@ -0,0 +1,23 @@
+export const PI = 3.141592653589793;
+export const TAU = 6.283185307179586;
+
+export const SQRT_2 = 1.4142135623730951;
+
+export const SIN_60 = 0.8660254037844386;
+export const COS_60 = 0.5;
+export const SIN_45 = 0.7071067811865476;
+export const COS_45 = 0.7071067811865476;
+export const SIN_30 = 0.5;
+export const COS_30 = 0.8660254037844386;
+
+export function degreesToRadians(degrees: number): number {
+  return degrees * (TAU / 360);
+}
+
+export function radiansToDegrees(radians: number): number {
+  return radians * (360 / TAU);
+}
+
+export function lerp(a: number, b: number, step: number): number {
+  return a + step * (b - a);
+}
