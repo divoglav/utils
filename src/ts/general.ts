1723f339554f7f0aa8d574e90553f3b3693216c4
divoglav <divoglav@protonmail.com>
2024-07-26 16:39:09 +0300

    feat: remove from array

--- src/ts/general.ts
+++ src/ts/general.ts
@@ -1,3 +1,8 @@
 export function wait(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+export function removeFromArray(array: any[], element: any) {
+  const indexToRemove = array.indexOf(element);
+  if (indexToRemove !== -1) array.splice(indexToRemove, 1);
+}
