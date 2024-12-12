027191a6a828918bedace6ceb952810df49ed8b7
divoglav <divoglav@protonmail.com>
2024-12-12 02:24:19 +0200

    update

--- src/ts/random.ts
+++
@@ -1,9 +0,0 @@
-export class Random {
-  static bool(): boolean {
-    return Math.random() > 0.5;
-  }
-
-  static range(from: number, to: number): number {
-    return from + Math.random() * (to - from);
-  }
-}
