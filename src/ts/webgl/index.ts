24f612f07d83d83acd80259973999f5e5f081e22
divoglav <divoglav@protonmail.com>
2024-12-24 02:50:49 +0200

    fix: minor

--- src/ts/webgl/index.ts
+++ src/ts/webgl/index.ts
@@ -2,3 +2,4 @@ export * as Canvas from "./canvas";
 export * as Kernels from "./kernels";
 export * as Points from "./points";
 export * as Setup from "./setup";
+export * as Texture from "./texture";
